b2c7e240557ccedcfdb1126ad1aacd6f08979846
Avery-Robinette <82397691+Avery-Robinette@users.noreply.github.com>
2021-05-10 10:21:36 -0500

    Semester project - avery coloring grid

--- script.js
+++ script.js
@@ -23,3 +23,23 @@ function fill() {
   .value;
   this.style.backgroundColor = color;
   line.style.backgroundColor = color;}
+
+var currentColor = "black"
+function onClick() {
+  event.srcElement.style.backgroundColor = currentColor
+}
+function colorButtonClick(color) {
+  currentColor = color
+}
+
+let div = document.getElementById("main-div")
+let gridDiv = document.createElement("div")
+gridDiv.classList.add("grid-container")
+for (let step = 0; step < 36; step++) {
+  let item = document.createElement("div")
+  item.classList.add("items")
+  item.addEventListener('click', onClick)
+  gridDiv.appendChild(item)
+}
+
+div.appendChild(gridDiv)
